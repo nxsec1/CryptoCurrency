f23066b79f626da2d6c0c51457dfbf52084c78ce
Karen <douk@student.unimelb.edu.au>
2020-08-13 22:05:33 +1000

    lombok working

--- src/main/java/com/fdm/CryptoCurrency/api/CurrencyDetail.java
+++ src/main/java/com/fdm/CryptoCurrency/api/CurrencyDetail.java
@@ -1,36 +1,17 @@
 package com.fdm.CryptoCurrency.api;
 
-import java.util.HashMap;
 import java.util.Map;
 
 import org.json.JSONObject;
 
-public class CurrencyDetail extends DataView {
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public  class CurrencyDetail extends DataView {
 	private String id;
 	private Map<String,Double> market_cap;
-	
-
-	public Map<String, Double> getMarket_cap() {
-		return market_cap;
-	}
-
-	public void setMarket_cap(Map<String, Double> market_cap) {
-		this.market_cap = market_cap;
-	}
-
-	public CurrencyDetail() {
-		super();
-		this.market_cap = new HashMap<>();
-	}
-	
-
-	public String getId() {
-		return id;
-	}
-
-	public void setId(String id) {
-		this.id = id;
-	}
 
 	@Override
 	public DataView processData(JSONObject obj) {
