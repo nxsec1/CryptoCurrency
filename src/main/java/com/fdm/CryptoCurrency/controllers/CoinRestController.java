2877478513cd52d764c30f2160bceda33e0ef5a6
Karen <douk@student.unimelb.edu.au>
2020-08-18 14:57:19 +1000

    validator working

--- src/main/java/com/fdm/CryptoCurrency/controllers/CoinRestController.java
+++ src/main/java/com/fdm/CryptoCurrency/controllers/CoinRestController.java
@@ -1,17 +1,23 @@
 package com.fdm.CryptoCurrency.controllers;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fdm.CryptoCurrency.service.CoinService;
 import com.fdm.CryptoCurrency.api.CryptoCurrency;
 import com.fdm.CryptoCurrency.api.CurrencyDetail;
+import com.fdm.CryptoCurrency.exception.NotFoundCurrencyException;
+import com.fdm.CryptoCurrency.exception.NotFoundPageException;
 
 @RestController
 @Validated
@@ -27,21 +33,39 @@ public class CoinRestController {
 	}
 	
 	@GetMapping(value = "coins/market")
-	public ArrayList<CryptoCurrency> getAll(@RequestParam(defaultValue = "usd",name="currency") String currency, @RequestParam(name="page") Integer page){
-		
-		ArrayList<CryptoCurrency> cryptoCurrencys = coinService.getAll(currency,Integer.toString(page));
-		
+	public ArrayList<CryptoCurrency> getAll(@RequestParam(defaultValue = "usd",name="currency") String currency, @RequestParam(defaultValue = "1",name="page") String page) throws NotFoundCurrencyException, NotFoundPageException{
+		List<String> avaliableCurrencies = new ArrayList<>(Arrays.asList("jpy", "aud","usd"));
+		if(!avaliableCurrencies.contains(currency.toLowerCase())) {
+			throw new NotFoundCurrencyException("Currency Not Found!");
+		}
+		if(Integer.parseInt(page)>10 || Integer.parseInt(page)<1) {
+			throw new NotFoundPageException();
+		}
+		ArrayList<CryptoCurrency> cryptoCurrencys = coinService.getAll(currency,page);
 		return cryptoCurrencys;
 	}
 	
 	
 	@GetMapping(value = "/coins/{id}")
 	public CurrencyDetail getCurrencyDetail(@PathVariable String id) {
-
 		CurrencyDetail currencyDetail = null;
-		
 		currencyDetail = coinService.getCurrencyDetail(id);
 		return currencyDetail;
 	}	
+	
+	@GetMapping(value = "/error")
+	public String error() {
+		return " No valid.\n Try http://localhost:8080/coins/market or http://localhost:8080/coins/{id} instead! \n Thanks!";
+	}	
+	
+	@ExceptionHandler(NotFoundCurrencyException.class)
+	public String notFoundCurrency() {
+		return "Currency Not Found! \nThe availiable currencies are JPY ,AUD , USD. \nTry http://localhost:8080/coins/market instead. \nThanks!";
+	}
+	
+	@ExceptionHandler(NotFoundPageException.class)
+	public String notFoundPage() {
+		return "Page Not Found! \nThe availiable page numbers are [1,10] \nTry http://localhost:8080/coins/market instead. \nThanks!";
+	}
 	
 }
