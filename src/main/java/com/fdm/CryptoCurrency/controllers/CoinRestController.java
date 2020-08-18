6ea8eb991804f98adfd3311284459327a5d5cfb6
Karen <douk@student.unimelb.edu.au>
2020-08-18 15:34:40 +1000

    clearing

--- src/main/java/com/fdm/CryptoCurrency/controllers/CoinRestController.java
+++ src/main/java/com/fdm/CryptoCurrency/controllers/CoinRestController.java
@@ -5,12 +5,10 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fdm.CryptoCurrency.service.CoinService;
@@ -20,8 +18,6 @@ import com.fdm.CryptoCurrency.exception.NotFoundCurrencyException;
 import com.fdm.CryptoCurrency.exception.NotFoundPageException;
 
 @RestController
-@Validated
-
 public class CoinRestController {
 	private CoinService coinService;
 	
